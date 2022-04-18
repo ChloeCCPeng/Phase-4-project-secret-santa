621a389e258737ceee2215990e9bfcfe7ad20a75
Chloe <chloepeng331@icloud.com>
2022-04-18 13:50:21 -0400

    app routes

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,33 @@ import './components/YourSecrect';
 
 function App() {
   return (
-
-    <div>App</div>
-  )
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <div class="home">
+            <Home />
+          </div>
+        </Route>
+        <Route exact path="/onlineStore">
+          <OnlineStore />
+        </Route>
+        <Route exact path="/signUp">
+          <SignUp />
+        </Route>
+        <Route exact path="/event">
+          <Event />
+        </Route>
+        <Route exact path="/invite">
+          <Invite />
+        </Route>
+        <Route exact path="/yourSecret">
+          <YourSecrect />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
+
 
 export default App
