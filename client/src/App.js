f1fe7ee849063e0ed099f10cfe3fb1ad5d191fc2
Chloe <chloepeng331@icloud.com>
2022-04-18 18:14:37 -0400

    home and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import './components/OnlineStore';
-import './components/SignUp';
-import './components/Event';
-import './components/Invite';
-import './components/YourSecret';
-import './components/Nav';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// import logo from './logo.svg';
+import './App.css';
+import OnlineStore from './components/OnlineStore.js';
+import Home from './components/Home.js';
+import Nav from './components/Nav.js';
+import SignUp from './components/SignUp.js';
+import Event from './components/Event.js';
+import Invite from './components/Invite.js';
+import YourSecret from './components/YourSecret.js';
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Switch>
-        <Route exact path="/">
-          <div class="home">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/">
             <Home />
-          </div>
-        </Route>
-        <Route exact path="/onlineStore">
-          <OnlineStore />
-        </Route>
-        <Route exact path="/signUp">
-          <SignUp />
-        </Route>
-        <Route exact path="/event">
-          <Event />
-        </Route>
-        <Route exact path="/invite">
-          <Invite />
-        </Route>
-        <Route exact path="/yourSecret">
-          <YourSecret />
-        </Route>
-      </Switch>
+          </Route>
+          <Route exact path="/onlineStore">
+            <OnlineStore />
+          </Route>
+          <Route exact path="/signUp">
+            <SignUp />
+          </Route>
+          <Route exact path="/event">
+            <Event />
+          </Route>
+          <Route exact path="/invite">
+            <Invite />
+          </Route>
+          <Route exact path="/yourSecret">
+            <YourSecret />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 
 export default App
