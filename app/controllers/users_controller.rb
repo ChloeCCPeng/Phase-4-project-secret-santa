23eb6c4887bdf48c0c5e9f3e8a1b30fbb1304960
LoopFruits <Kensworldc@gmail.com>
2022-04-19 10:12:53 -0400

    sessions create and destroy routes, users show routes, as well as the corresponding controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,36 @@
 class UsersController < ApplicationController
     #getting the users data from the backend into state when the page first loads 
+    skip_before_action :authorized, only: [:create]
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
     def show
-      user = User.find_by(id: session[:user_id])
-      if user
-        render json: user
-      else
-        render json: { error: "Not authorized" }, status: :unauthorized
-      end
+      current_user = User.find(session[:user_id])
+      render json: current_user
     end
+
+
+    def create 
+      user = User.create!(user_params)
+      render json: user
+    end
+
+    private 
+
+
+    def render_unprocessable_entity(invalid)
+      render json: {error: invlaid.record.errors}, status: :unprocessable_entity
+    end
+
+    def user_params 
+      params.permit(:username, :password)
+    end
+
+
+    #strong param to authorize certain users to acess the page 
+    def authorize 
+      return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+    end
+
+
+
   end
