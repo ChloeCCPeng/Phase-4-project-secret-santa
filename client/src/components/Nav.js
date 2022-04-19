7138a07312a06befdb0273486f356fd7ee468703
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-04-18 21:15:58 -0400

    Update Nav.js

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function Nav() {
+// import logo from './logo.svg';
+import './App.css';
+import OnlineStore from './components/OnlineStore.js';
+import Home from './components/Home.js';
+import Nav from './components/Nav.js';
+import SignUp from './components/SignUp.js';
+import Event from './components/Event.js';
+import Invite from './components/Invite.js';
+import YourSecret from './components/YourSecret.js';
+
+function App() {
   return (
-    <div>Nav</div>
-  )
+    <div className="App">
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/">
+          </Route>
+          <Route exact path="/onlineStore">
+            <OnlineStore />
+          </Route>
+          <Route exact path="/signUp">
+            <SignUp />
+          </Route>
+          <Route exact path="/event">
+            <Event />
+          </Route>
+          <Route exact path="/invite">
+            <Invite />
+          </Route>
+          <Route exact path="/yourSecret">
+            <YourSecret />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default Nav
+export default App
