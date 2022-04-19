e5a8a16f98b29a8a2f91bb0b3bcea307c008feaf
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-04-19 15:06:11 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,25 @@ Wishlist.destroy_all
 puts " Seeding data...."
 
 puts " 👥 Creating users "
-User.create(first_name:"Maggie", last_name: "Lee", email: "maggie@abc.com", password: "maggie")
-User.create(first_name:"Jackie", last_name: "Jordan", email: "jackie@abc.com", password: "maggie")
-User.create(first_name:"Bryan", last_name: "Thao", email: "bryan@abc.com", password: "maggie")
-User.create(first_name:"Chloe", last_name: "White", email: "chloe@abc.com", password: "maggie")
-User.create(first_name:"Dennis", last_name: "Mercer", email: "dennis@abc.com", password: "maggie")
+User.create(id: 1, first_name:"Maggie", last_name: "Lee", email: "maggie@abc.com", password: "maggie")
+User.create(id: 2, first_name:"Jackie", last_name: "Jordan", email: "jackie@abc.com", password: "maggie")
+User.create(id: 3, first_name:"Bryan", last_name: "Thao", email: "bryan@abc.com", password: "maggie")
+User.create(id: 4, first_name:"Chloe", last_name: "White", email: "chloe@abc.com", password: "maggie")
+User.create(id: 5, first_name:"Dennis", last_name: "Mercer", email: "dennis@abc.com", password: "maggie")
 
 puts " 🎁 Creating gifts "
-Gift.create(gift: "mouse")
-Gift.create(gift: "travel mug")
-Gift.create(gift: "hiking backpack")
-Gift.create(gift: "keyboard")
-Gift.create(gift: "wholefood gift card")
+Gift.create(id: 1, item: "mouse")
+Gift.create(id: 2, item: "travel mug")
+Gift.create(id: 3, item: "hiking backpack")
+Gift.create(id: 4, item: "keyboard")
+Gift.create(id: 5, item: "wholefood gift card")
 
 puts " 🎟️ Creating events "
-Event.create(date: "12.23.2022", deadline: "12.18.2022", patispate: "in-person", budget: 50, virtual_link: "zoom.com" rules: "Try to stay within the spending limit, and make sure you make a Wishlist to give your person some good ideas. This is going to be lots of fun!")
+Event.create(id: 1, date: "12.23.2022", deadline: "12.18.2022", patispate: "in-person", budget: 50, virtual_link: "zoom.com", rules: "Try to stay within the spending limit, and make sure you make a Wishlist to give your person some good ideas. This is going to be lots of fun!")
 
 puts " 🎁 Creating wishlist "
-Wishlist.create(wish1: "backpack", wish2: "travel mug", wish3: "sofa potato blanket", wish4: "air fryer", wish5: "candles")
-Wishlist.create(wish1: "3D puzzles", wish2: "travel mug", wish3: "Apple air tags", wish4: "Silk Eyemask", wish5: "giftcards")
-Wishlist.create(wish1: "backpack", wish2: "travel mug", wish3: "sofa potato blanket", wish4: "air fryer", wish5: "candles")
-Wishlist.create(wish1: "3D puzzles", wish2: "travel mug", wish3: "Apple air tags", wish4: "Silk Eyemask", wish5: "giftcards")Wishlist.create(wish1: "3D puzzles", wish2: "travel mug", wish3: "Apple air tags", wish4: "Silk Eyemask", wish5: "giftcards")
+Wishlist.create(id: 1, wish1: "backpack", wish2: "travel mug", wish3: "sofa potato blanket", wish4: "air fryer", wish5: "candles")
+Wishlist.create(id: 2, wish1: "3D puzzles", wish2: "travel mug", wish3: "Apple air tags", wish4: "Silk Eyemask", wish5: "giftcards")
+Wishlist.create(id: 3, wish1: "backpack", wish2: "travel mug", wish3: "sofa potato blanket", wish4: "air fryer", wish5: "candles")
+Wishlist.create(id: 4, wish1: "3D puzzles", wish2: "travel mug", wish3: "Apple air tags", wish4: "Silk Eyemask", wish5: "giftcards")
+Wishlist.create(id: 5, wish1: "3D puzzles", wish2: "travel mug", wish3: "Apple air tags", wish4: "Silk Eyemask", wish5: "giftcards")
