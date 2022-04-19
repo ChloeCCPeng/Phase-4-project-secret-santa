b810bfaa61e149c4a633007eb9d8577d87dd51b1
Chloe <chloepeng331@icloud.com>
2022-04-19 14:27:28 -0400

    creating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-puts " Bee-Bop Seeding data...."
+User.destroy_all
+Gift.destroy_all
 
+puts " Seeding data...."
 
+puts " 👥 Creating users "
+User.create(first_name:"Maggie", last_name: "Lee", email: "maggie@abc.com", password: "maggie")
+User.create(first_name:"Jackie", last_name: "Jordan", email: "jackie@abc.com", password: "maggie")
+User.create(first_name:"Bryan", last_name: "Thao", email: "bryan@abc.com", password: "maggie")
+User.create(first_name:"Chloe", last_name: "White", email: "chloe@abc.com", password: "maggie")
+User.create(first_name:"Dennis", last_name: "Mercer", email: "dennis@abc.com", password: "maggie")
 
-puts "🤑 We're cash money"
+puts " 🎁 Creating gifts "
+Gift.create(gift: "mouse")
+Gift.create(gift: "travel mug")
+Gift.create(gift: "hiking backpack")
+Gift.create(gift: "keyboard")
+Gift.create(gift: "wholefood gift card")
+
+puts " 🎟️ Creating events "
+Event.create(date: "12.23.2022", deadline: "12.18.2022", patispate: "in-person", budget: 50, virtual_link: "zoom.com" rules: "Try to stay within the spending limit, and make sure you make a Wishlist to give your person some good ideas. This is going to be lots of fun!")
+
+uts " 🎁 Creating wishlist "
+Wishlist.create(wish1: "backpack", wish2: "travel mug", wish3: "sofa potato blanket", wish4: "air fryer", wish5: "candles")
+Wishlist.create(wish1: "3D puzzles", wish2: "travel mug", wish3: "Apple air tags", wish4: "Silk Eyemask", wish5: "giftcards")
+Wishlist.create(wish1: "backpack", wish2: "travel mug", wish3: "sofa potato blanket", wish4: "air fryer", wish5: "candles")
+Wishlist.create(wish1: "3D puzzles", wish2: "travel mug", wish3: "Apple air tags", wish4: "Silk Eyemask", wish5: "giftcards")Wishlist.create(wish1: "3D puzzles", wish2: "travel mug", wish3: "Apple air tags", wish4: "Silk Eyemask", wish5: "giftcards")
