894392a52883f20a87fe1f638e9c7550a8ded9f3
Chloe <chloepeng331@icloud.com>
2022-04-19 14:43:14 -0400

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 User.destroy_all
 Gift.destroy_all
+Event.destroy_all
+Wishlist.destroy_all
 
 puts " Seeding data...."
 
@@ -20,7 +22,7 @@ Gift.create(gift: "wholefood gift card")
 puts " 🎟️ Creating events "
 Event.create(date: "12.23.2022", deadline: "12.18.2022", patispate: "in-person", budget: 50, virtual_link: "zoom.com" rules: "Try to stay within the spending limit, and make sure you make a Wishlist to give your person some good ideas. This is going to be lots of fun!")
 
-uts " 🎁 Creating wishlist "
+puts " 🎁 Creating wishlist "
 Wishlist.create(wish1: "backpack", wish2: "travel mug", wish3: "sofa potato blanket", wish4: "air fryer", wish5: "candles")
 Wishlist.create(wish1: "3D puzzles", wish2: "travel mug", wish3: "Apple air tags", wish4: "Silk Eyemask", wish5: "giftcards")
 Wishlist.create(wish1: "backpack", wish2: "travel mug", wish3: "sofa potato blanket", wish4: "air fryer", wish5: "candles")
