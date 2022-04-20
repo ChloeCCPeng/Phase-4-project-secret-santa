a61f6fdadcd0192cd85ef6ea3003fa5ceede9a03
Chloe <chloepeng331@icloud.com>
2022-04-20 15:44:07 -0400

    CRUD for event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,26 @@
 class EventsController < ApplicationController
   def index
+    render json: Event.all
   end
 
   def create
+    event = Event.create(event_params)
+    render json: event, status: :created
   end
 
   def show
+    event = find_event
+    render json: event
   end
+
+  private
+  
+  def event_params
+    params.permit(:date, :deadline, :participate, :budget, :virtual_link, :rules, :user_id)
+  end
+
+  def find_event
+    Event.find(params[:id])
+  end
+
 end
