5e4cb8c9d8efa98826d967831feb5d6370c89dcf
Chloe <chloepeng331@icloud.com>
2022-04-21 14:40:24 -0400

    add signup route on home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,17 +4,22 @@ import Event from './Event';
 import Nav from './Nav';
 // import Invite from './Invite';
 import SignUp from './SignUp';
+import { NavLink } from 'react-router-dom'  
 
 function Home() {
   return (
     <div>
       <h2 id="Secret-Santa">Secret Santa</h2>
       <img id="Secret-Santa-image" src="https://woodsboroworld.com/wp-content/uploads/2021/03/secret-santa-claus-invitation-background-standing-blank-sign-showing-big-blank-sign_209425-431.jpg" alt="Secret Santa" />
-      <button type="button">Start Secret Santa!</button>
-      <OnlineStore />
+      <NavLink to="/SignUp">
+        <button type="button">Start Secret Santa!</button>
+      </NavLink>
+      
+      
+      {/* <OnlineStore />
       <Event />
       <Nav />
-      <SignUp />
+      <SignUp /> */}
       {/* <Invite /> */}
     </div>
   )
