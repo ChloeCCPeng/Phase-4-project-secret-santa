32c0ef8e6ebb3b76d6de63c479ac4a9cc87b74ff
Chloe <chloepeng331@icloud.com>
2022-04-21 11:31:10 -0400

    commands out useEffect on YourSecret to fix the bugs

--- client/src/components/YourSecret.js
+++ client/src/components/YourSecret.js
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from 'react'
 function YourSecret() {
 
   const [secret, setSecret] = useState([])
-  const API2 = '/secret'
+  // const API2 = '/secret'
 
-  useEffect(() => {
-    fetch(API2)
-      .then((res) => res.json())
-      .then((data) => setSecret(data))
-  }, [])
+  // useEffect(() => {
+  //   fetch(API2)
+  //     .then((res) => res.json())
+  //     .then((data) => setSecret(data))
+  // }, [])
 
   return (
     <div>
